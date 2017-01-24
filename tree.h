6883a189e20204e1381b3879103b8e5611d5ada1
Michał Izworski <m.izworski7@gmail.com>
2017-01-24 20:43:49 +0100

    Methods declared.

--- tree.h
+++ tree.h
@@ -2,5 +2,95 @@
 #define JNP7_TREE_H
 
 
+#include <memory>
+#include <stack>
+
+template<typename T>
+class Tree {
+    using tsize = size_t;
+    using tree_ptr = std::shared_ptr<Tree<T>>;
+    using tnode = Tree<T>;
+public:
+    Tree() {}
+
+    Tree(tree_ptr root) {
+
+    }
+
+    Tree(const Tree &other) {
+
+    }
+//    ~Tree() {}
+
+    // todo const T& instead of T?
+    T fold(std::function<T(T, T, T)> operation, T init) {
+        return init;
+    }
+
+    Tree<T> filter(std::function<bool(T)> predicate) {
+        return *this;
+    }
+
+    Tree<T> map(std::function<T(T)> transformer) {
+        return *this;
+    }
+
+    Tree<T> lazy_map(std::function<T(T)> transformer) {
+        return *this;
+    }
+
+    T accumulate(std::function<T(T, T)> operation,
+                 T init,
+                 std::function<tnode(tnode, std::stack<tnode> &)> traversal) { //todo traversal type?
+        return init;
+    }
+
+    void apply(std::function<void(T)> operation, std::function<tnode(tnode, std::stack<tnode> &)> traversal) {
+
+    }
+
+    tsize height() {
+        return 0;
+    }
+
+    tsize size() {
+        return 0;
+    }
+
+    bool is_bst() {
+        return false;
+    }
+
+    void print(std::function<tnode(tnode, std::stack<tnode> &)> traversal = Tree<T>::inorder) {
+
+    }
+
+    static tnode preorder(tnode node, std::stack<tnode> nodes_to_traverse) { //todo dlaczego nie stack &?
+        return Tree<T>();
+    }
+
+    static tnode inorder(tnode node, std::stack<tnode> nodes_to_traverse) {
+        return Tree<T>();
+    }
+
+    static tnode postorder(tnode node, std::stack<tnode> nodes_to_traverse) {
+        return Tree<T>();
+    }
+
+    static tnode createEmptyNode() {
+        return Tree<T>();
+    }
+
+    static tnode createValueNode(T value) {
+        return Tree<T>();
+    }
+
+    static tnode createValueNode(T value, tnode left, tnode right) {
+        return Tree<T>();
+    }
+
+private:
+
+};
 
 #endif //JNP7_TREE_H
--- tree_example.cc
+++ tree_example.cc
@@ -6,7 +6,6 @@
 #include "tree.h"
 
 int main(int argc, const char * argv[]) {
-    /*
     auto two = Tree<int>::createValueNode(2);
     auto one = Tree<int>::createValueNode(1, Tree<int>::createEmptyNode(), two);
     auto four = Tree<int>::createValueNode(4);
@@ -16,9 +15,11 @@ int main(int argc, const char * argv[]) {
     auto root = Tree<int>::createValueNode(3, one, five);
     Tree<int> tree(root);
 
-    tree.print(Tree<int>::inorder);
-    tree.print(Tree<int>::preorder);
-    tree.print(Tree<int>::postorder);
+    Tree<int>::g();
+
+//    tree.print(Tree<int>::inorder);
+//    tree.print(Tree<int>::preorder);
+//    tree.print(Tree<int>::postorder);
 
     std::cout << "size: " << tree.size() << std::endl;
     std::cout << "height: " << tree.height() << std::endl;
@@ -33,19 +34,19 @@ int main(int argc, const char * argv[]) {
     Tree<int> lazy_minus_x = tree.lazy_map([&](int e)->int{return e - x;});
     x = 4;
     std::cout << "-2: ";
-    minus_x.print();
+//    minus_x.print();
     std::cout << "-4: ";
-    lazy_minus_x.print();
+//    lazy_minus_x.print();
 
     Tree<int> odd = minus_x.filter([](int e)->bool{return (e & 1) == 1;});
     std::cout << "odd: ";
-    odd.print();
+//    odd.print();
 
     int sum1 = tree.accumulate(std::plus<int>(), 0, Tree<int>::inorder);
     std::cout << "sum: " << sum1 << std::endl;
 
     int sum2 = tree.fold([](int val, int l, int r)->int{return val + l + r;}, 0);
     std::cout << "sum: " << sum2 << std::endl;
-     */
+
     return 42;
 }
